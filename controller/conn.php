f0bbf3720270ddfb713d884d941cda08c11e604b
KhyeShen <99790267+KhyeShen@users.noreply.github.com>
2022-10-22 16:17:40 +0800

    conn

--- controller/conn.php
+++ controller/conn.php
@@ -1,7 +1,6 @@
 <?php
-//  $conn = mysqli_connect("my","bscfypwb_khyeshen","G^MPB##NaAf7","bscfypwb_E-library");
-$conn = mysqli_connect("localhost","root","","elibrary");
- 
+  $conn = mysqli_connect("my","bscfypwb_khyeshen","G^MPB##NaAf7","bscfypwb_E-library");
+
 if (mysqli_connect_errno())
   {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
