a979d21d296ee6853507c5fcba0b559a52660e58
KhyeShen <99790267+KhyeShen@users.noreply.github.com>
2022-10-29 11:58:15 +0800

    cron

--- student/trenew.php
+++ student/trenew.php
@@ -3,8 +3,10 @@
 
     date_default_timezone_set("Asia/Kuala_Lumpur");
     $currentDT = date("Y-m-d h:i:s");
-    $sql = "INSERT INTO payment(student_ID,stripe_subscription_ID,amount,payment_datetime,status) VALUES
-					('SCPG1800369','sub_1LvY5UEYRt4577ibifZxWEHt',10,'".$currentDT."','Suceed')";
+    // $sql = "INSERT INTO payment(student_ID,stripe_subscription_ID,amount,payment_datetime,status) VALUES
+	// 				('SCPG1800369','sub_1LvY5UEYRt4577ibifZxWEHt',10,'".$currentDT."','Suceed')";
                     
-                    $insert = $conn->query($sql); 
+    //                 $insert = $conn->query($sql); 
+    $sql = "UPDATE subscription SET status='expired' WHERE student_ID='SCPG1800369'";
+    $insert = $conn->query($sql); 
 ?>
