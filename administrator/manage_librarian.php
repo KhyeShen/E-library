bcd2f2b079ff639969f92b8ce681f387868cde00
KhyeShen <99790267+KhyeShen@users.noreply.github.com>
2022-11-25 15:13:19 +0800

    Update manage_librarian.php

--- administrator/manage_librarian.php
+++ administrator/manage_librarian.php
@@ -153,7 +153,7 @@ include('../controller/conn.php');
                 <div class="" style="margin-top: 17px;">
                     <b>Password</b>
                     <div class="col">
-                        <input class="form-control" type="password" name="password" placeholder="Password" style="margin-top: 3px;" autocomplete="new-password" required>
+                        <input class="form-control" type="password" name="password" placeholder="Password" style="margin-top: 3px;" autocomplete="off" autocomplete="new-password" required>
                     </div>
                 </div>
                 <div class="" style="margin-top: 14px; text-align:right;">
