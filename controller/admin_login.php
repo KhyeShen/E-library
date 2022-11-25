acb7deefd4bac076dd76ab2a695aaf20ced2027b
KhyeShen <99790267+KhyeShen@users.noreply.github.com>
2022-11-25 14:51:45 +0800

    Update admin_login.php

--- controller/admin_login.php
+++ controller/admin_login.php
@@ -7,9 +7,16 @@
 	//student login
     if(isset($_POST['login']))
 	{
-		if (isset($_COOKIE['email'])) {
-			unset($_COOKIE['email']); 
-		}
+		// unset cookies
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
 
 		if(isset($_POST['email']))
 		{
