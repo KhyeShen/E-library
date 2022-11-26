fd11c145dbda7f86ef7c9d9cae3155687faf0fc9
KhyeShen <99790267+KhyeShen@users.noreply.github.com>
2022-11-26 11:13:55 +0800

    update

--- librarian/update_form.php
+++ librarian/update_form.php
@@ -41,6 +41,7 @@ if(isset($_GET['material_ID'])){
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
   <!-- Bootstrap CSS -->
   <link href="../src/css/bootstrap_dashboard.css" rel="stylesheet">
+  <link rel="stylesheet" href="../src/css/bootstrap.min.css">
   <!-- Dashboard CSS -->
   <link href="../src/css/dashboard.css" rel="stylesheet">
   <!-- Font Awesome -->
