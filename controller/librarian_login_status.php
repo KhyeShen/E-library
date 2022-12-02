adbf174d9358094c46e7815c819da542aae79718
KhyeShen <99790267+KhyeShen@users.noreply.github.com>
2022-12-03 00:11:57 +0800

    Update librarian_login_status.php

--- controller/librarian_login_status.php
+++ controller/librarian_login_status.php
@@ -1,6 +1,6 @@
 <?php
     //check if admin login
-    if (!isset($_SESSION['librarian_ID']) ||(trim ($_SESSION['librarian_ID']) == '') || $_SESSION['loginstatus'] != 'active') {
+    if (!isset($_SESSION['librarian_ID']) ||(trim ($_SESSION['librarian_ID']) == '') || $_SESSION['librarian_loginstatus'] != 'active') {
         echo '<script type="text/javascript">'; 
         echo 'alert("Please Login Your Account");'; 
         echo 'window.location.href = "../librarian/index.php";';
