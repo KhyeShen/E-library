9d71e06a812683cb9b4786cd1238c748798f6e24
KhyeShen <99790267+KhyeShen@users.noreply.github.com>
2022-12-26 14:27:50 +0800

    Update renew_subscription.php

--- controller/renew_subscription.php
+++ controller/renew_subscription.php
@@ -14,24 +14,26 @@
     $sql_subscription = "SELECT * FROM subscription";
     $query_subscription = mysqli_query($conn, $sql_subscription);
     while ($subscription = mysqli_fetch_array($query_subscription)) {
+        $subscription_ID = $subscription['stripe_subscription_ID'];
+
+        //Check if the subscription need to updated
         if($subscription['plan_end'] <= $currentDT)
         {
             if($subscription['status'] == "last")
             {
                 $update = "UPDATE subscription SET status='expired', updated_datetime='".$currentDT."' WHERE stripe_subscription_ID='".$subscription_ID."'";
-                $update_student = "UPDATE student set subscription='None', updated_datetime='$currentDT' WHERE student_ID = '".$_SESSION['studentID']."'";
+                $update_student = "UPDATE student SET subscription='None', updated_datetime='".$currentDT."' WHERE student_ID = '".$_SESSION['studentID']."'";
                 mysqli_query($conn,$update);
                 mysqli_query($conn,$update_student);
             }
             else if($subscription['status'] == "active")
             {
                 $newexpiry = date('Y-m-d', strtotime( $subscription['plan_end'] . " +1 month"));
-                $subscription_ID = $subscription['stripe_subscription_ID'];
                 $amount = $subscription['monthly_price'];
                 $new = "INSERT INTO payment(student_ID,stripe_subscription_ID,amount,payment_datetime,status) VALUES
                         ('".$_SESSION['studentID']."','".$subscription_ID."','".$amount."','".$currentDT."','Suceed')";
                 mysqli_query($conn,$new);
-                $update = "UPDATE subscription SET status='active',plan_end='".$newexpiry."' WHERE stripe_subscription_ID='".$subscription_ID."'";
+                $update = "UPDATE subscription SET status='active',plan_end='".$newexpiry."', updated_datetime='$currentDT' WHERE stripe_subscription_ID='".$subscription_ID."'";
                 $update_student = "UPDATE student set subscription='Premium', updated_datetime='$currentDT' WHERE student_ID = '".$_SESSION['studentID']."'";
                 mysqli_query($conn,$update);
                 mysqli_query($conn,$update_student);
